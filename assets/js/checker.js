a0b87cb6e96625f8fa7b799cda5f413ebc461256
Lisha Sterling <lishevita@gmail.com>
2013-11-02 03:51:59 -0700

    oops, forgot to add checker.js in my commit

---
+++ assets/js/checker.js
@@ -0,0 +1,56 @@
+function checkResponse(praise, encourage){
+  var num_rows=localStorage.length;
+  var key="userdata."+(num_rows).toString();
+  // My test with json
+  var myinfo = JSON.parse(localStorage[key]);
+  myinfo[5] = myinfo[5]+1;
+
+    var answer = doDM();
+
+    if(answer == "1"){  
+  //correct
+  e = document.getElementById('output');
+      e.innerHTML= praise;
+
+
+  myinfo[6] = 1 ;
+
+
+  // start of jquery button stuff for right answers
+    $("#right").css("visibility","visible");
+
+    $(function(){
+       $("#harder").click(function(){
+         window.location = "cipher2.htm";
+      });
+       $("#finished").click(function(){
+         window.location = "index.htm";
+      });
+    });
+
+  }
+  else{
+  //failed
+  e = document.getElementById('output');
+      e.innerHTML= encourage;
+
+  $("#wrong").css("visibility","visible");
+
+  // start of jquery button stuff
+    $(function() {
+        $("#tryagain").click(function(){
+           window.location = "cipher.htm";
+        });
+        $("#giveup").click(function(){
+            window.location = "index.htm";
+        });           
+       $("#hint").click(function(){
+            $("#hintbox").css("display","block");
+       });
+    });
+
+
+  }
+  textToAdd=JSON.stringify(myinfo);
+  localStorage[key]=textToAdd;
+}
